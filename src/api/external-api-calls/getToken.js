17c6f6253ca1a633aeaf662404fcf16992115007
Patrick McLaughlin <patrickmc21@gmail.com>
2018-04-09 19:09:16 -0600

    add getToken tests phase 2

--- src/api/external-api-calls/getToken.js
+++ src/api/external-api-calls/getToken.js
@@ -1,26 +1,30 @@
 import { clientId, clientSecret } from '../apiKeys/strava-keys';
 
 const getToken = async (temporaryToken) => {
-    const url = '/tokenexchange';
-    const options = {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        mode: 'cors',
-        cache: 'no-cache',
-        body: JSON.stringify({
-            token: temporaryToken,
-            clientId: clientId,
-            clientSecret: clientSecret
-        })
-    }
+  const url = '/tokenexchange';
+  const options = {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    mode: 'cors',
+    cache: 'no-cache',
+    body: JSON.stringify({
+      token: temporaryToken,
+      clientId: clientId,
+      clientSecret: clientSecret
+    })
+  }
 
-    const response = await fetch(url, options)
+  try {
+    const response = await fetch(url, options);
     const data = await response.json();
     const athleteInfo = JSON.parse(data)
     return athleteInfo;
+  } catch (error) {
+    throw error.message;
+  }
 }
 
 export default getToken
