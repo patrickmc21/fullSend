6b6f6f0402abcd4d5a455a912f1bc643e89c5d67
Patrick McLaughlin <patrickmc21@gmail.com>
2018-04-09 19:22:56 -0600

    Add getTrails tests phase 2

--- src/api/external-api-calls/getTrails.js
+++ src/api/external-api-calls/getTrails.js
@@ -2,8 +2,13 @@ import key from '../apiKeys/mtb-project-key.js';
 
 export const getTrails = async (lat, long) => {
   const rootUrl = 'https://www.mtbproject.com/data/get-trails?';
-  const urlOptions = `lat=${lat}&lon=${long}&maxDistance=0&sort=distance&key=${key}`
-  const response = await fetch(`${rootUrl}${urlOptions}`);
-  const data = await response.json();
-  return data;
+  const urlOptions = `lat=${lat}&lon=${long}&maxDistance=0&sort=distance&key=${key}`;
+
+  try {
+    const response = await fetch(`${rootUrl}${urlOptions}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw error.message;
+  }
 }
