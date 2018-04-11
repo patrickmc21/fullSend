180c068d588e7601617452eca36f4da55e98c9f9
Patrick McLaughlin <patrickmc21@gmail.com>
2018-04-10 19:04:04 -0600

    lint maincontent component phase 2

--- src/Components/MainContent/MainContent.js
+++ src/Components/MainContent/MainContent.js
@@ -8,12 +8,12 @@ const MainContent = () => {
       <div className='main-content'>
         <div className='main-content-text'>
           <h1 className='main-tagline'>
-            The Rider's Best Friend
+            The Rider&apos;s Best Friend
           </h1>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default MainContent;
