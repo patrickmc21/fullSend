89d24be7a38ec1f15a244d6edad8bbfbd367264b
Patrick McLaughlin <patrickmc21@gmail.com>
2018-04-13 16:29:18 -0600

    style rider stats page

--- src/Containers/RiderStats/RiderStats.js
+++ src/Containers/RiderStats/RiderStats.js
@@ -9,42 +9,44 @@ import './RiderStats.css';
 export const RiderStats = ({stats, user}) => {
   return (
     <section className='rider-stats'>
-      <article className='rider-profile'>
-        <div className='rider-info'>
-          <img 
-            src={user.img} 
-            alt='user profile'
-            className='user-profile-picture-stats' />
-          <h4 className='user-name-stats'>{user.name}</h4>
-          <h5 className='user-location-stats'>{user.location}</h5>
-        </div>
-        <div className='rider-records'>
-          <h3>All-Time Records</h3>
-          <p>Biggest Ride: {stats.biggestRideDistance}</p>
-          <p>Biggest Climb: {stats.biggestElevation}</p>
-        </div>
-      </article>
-      <article className='rider-recent-stats'>
-        <h3>Recent Rides</h3>
-        <p>Rides: {stats.recentRides.count}</p>
-        <p>Distance: {stats.recentRides.distance}</p>
-        <p>Time: {stats.recentRides.time}</p>
-        <p>Gain: {stats.recentRides.gain}</p>
-      </article>
-      <article className='rider-year-stats'>
-        <h3>Year to Date</h3>
-        <p>Rides: {stats.yearToDate.count}</p>
-        <p>Distance: {stats.yearToDate.distance}</p>
-        <p>Time: {stats.yearToDate.time}</p>
-        <p>Gain: {stats.yearToDate.gain}</p>
-      </article>
-      <article className='rider-life-stats'>
-        <h3>Lifetime</h3>
-        <p>Rides: {stats.lifetime.count}</p>
-        <p>Distance: {stats.lifetime.distance}</p>
-        <p>Time: {stats.lifetime.time}</p>
-        <p>Gain: {stats.lifetime.gain}</p>
-      </article>
+      <div className='stats-container'>
+        <article className='rider-profile'>
+          <div className='rider-info'>
+            <img
+              src={user.img} 
+              alt='user profile'
+              className='user-profile-picture-stats' />
+            <h4 className='user-name-stats'>{user.name}</h4>
+            <h5 className='user-location-stats'>{user.location}</h5>
+          </div>
+          <div className='rider-records'>
+            <h3>All-Time Records</h3>
+            <p>Biggest Ride: {stats.biggestRideDistance}</p>
+            <p>Biggest Climb: {stats.biggestElevation}</p>
+          </div>
+        </article>
+        <article className='rider-recent-stats'>
+          <h3>Recent Rides</h3>
+          <p>Rides: {stats.recentRides.count}</p>
+          <p>Distance: {stats.recentRides.distance}</p>
+          <p>Time: {stats.recentRides.time}</p>
+          <p>Gain: {stats.recentRides.gain}</p>
+        </article>
+        <article className='rider-year-stats'>
+          <h3>Year to Date</h3>
+          <p>Rides: {stats.yearToDate.count}</p>
+          <p>Distance: {stats.yearToDate.distance}</p>
+          <p>Time: {stats.yearToDate.time}</p>
+          <p>Gain: {stats.yearToDate.gain}</p>
+        </article>
+        <article className='rider-life-stats'>
+          <h3>Lifetime</h3>
+          <p>Rides: {stats.lifetime.count}</p>
+          <p>Distance: {stats.lifetime.distance}</p>
+          <p>Time: {stats.lifetime.time}</p>
+          <p>Gain: {stats.lifetime.gain}</p>
+        </article>
+      </div>
     </section>
   )
 }
