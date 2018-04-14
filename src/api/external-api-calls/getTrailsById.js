a92c833db132479b0c4acc3b03f602d9501f3c45
Patrick McLaughlin <patrickmc21@gmail.com>
2018-04-14 13:45:58 -0600

    remove unused strava images

--- src/api/external-api-calls/getTrailsById.js
+++ src/api/external-api-calls/getTrailsById.js
@@ -1,7 +1,6 @@
 import key from '../apiKeys/mtb-project-key.js';
 
 const getTrailsById = async (ids) => {
-  console.log(ids);
   const idString = ids.toDos.join(',');
   const rootUrl = 'https://www.mtbproject.com/data/get-trails-by-id';
   const urlOptions = `?ids=${idString}&key=${key}`;
@@ -10,7 +9,6 @@ const getTrailsById = async (ids) => {
   try {
     const response = await fetch(endpoint);
     const trails = await response.json();
-    console.log(trails)
     return trails;
   } catch (error) {
     throw error.message;
