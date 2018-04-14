c5e1c70fee59a311861922405ea3573e1c6ed7f1
Patrick McLaughlin <patrickmc21@gmail.com>
2018-04-14 16:13:57 -0600

    Add BikeCard tests phase 4

--- src/Components/BikeCard/BikeCard.js
+++ src/Components/BikeCard/BikeCard.js
@@ -2,17 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { convertMetersToMiles } from '../../api/helpers/rideCleaner';
 
-
+import defaultImg from './images/default.jpeg'
 import './BikeCard.css';
 
 const BikeCard = ({bike}) => {
   const distance = convertMetersToMiles(bike.distance);
-
+  const img = bike.description.includes('https://') ? bike.description : defaultImg;
   return (
     <article className='bike-card'>
       <img
         className='bike-img'
-        src={bike.description}
+        src={img}
         alt='bike image' />
       <h3 className='bike-name'>"{bike.name}"</h3>
       <h4 className='bike-brand'>{bike.brand_name}</h4>
