48f0f737de695e3d79ca0827165b4de8f832cb1d
Patrick McLaughlin <patrickmc21@gmail.com>
2018-05-04 11:24:21 -0600

    modifiy strava auth functionality for deployment

--- src/api/external-api-calls/getAthlete.js
+++ src/api/external-api-calls/getAthlete.js
@@ -1,6 +1,6 @@
 export const redirectLogin = () => {
   const url = 'https://www.strava.com/oauth/authorize';
   const urlOptions = '?client_id=24677&response_type=code&';
-  const redirect = 'redirect_uri=http://http://fullsend.surge.sh/&approval_prompt=force';
+  const redirect = 'redirect_uri=http://fullsend.surge.sh/&approval_prompt=force';
   window.location = `${url}${urlOptions}${redirect}`;
 };
